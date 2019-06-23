ea248098aea08c4e062ecab757b72c2ef4745fc2
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-23 14:20:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,5 +26,5 @@ end
     # To make this test pass, make sure you are iterating through your badges and room assignments lists.
 
 def printer(attendes)
-  
+  puts "#{batch_badge_creator} #{assign_rooms}"
 end
